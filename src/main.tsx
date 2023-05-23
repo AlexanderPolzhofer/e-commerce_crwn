230ab22abbda8e2c4a9a39435098bcdb4ac40a92
AlexanderPolzhofer <alexander.polzhofer@gmail.com>
2023-05-23 22:15:17 +0200

    init Global from emotion

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import { Global, css } from '@emotion/react'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <Global
+      styles={css`
+        * {
+          font-family: 'Roboto Condensed';
+        }
+      `}
+    />
     <App />
   </React.StrictMode>,
 )
